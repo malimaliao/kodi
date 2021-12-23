9257de29e546319bc4488840df4d3e5a56983ff0
malimaliao <dd9931@outlook.com>
2021-12-23 17:59:27 +0800

    update

--- plugin.video.duolasousuo/main.py
+++ plugin.video.duolasousuo/main.py
@@ -1,65 +1,236 @@
 # -*- coding:utf-8 -*-
-import re
+import sys, re, json
 import requests
 import urllib.parse
 from collections import OrderedDict
-import xbmcplugin, xbmcgui
-import json
+# https://codedocs.xyz/xbmc/xbmc/
+import xbmcplugin, xbmcgui, xbmc
 
 # plugin config
 _plugin_name = '哆啦搜索'
+_plugin_player_mimes = ['.m3u8','.mp4','.flv','.ts']
 _plugin_handle = int(sys.argv[1])  # 当前句柄
 _plugin_address = sys.argv[0]  # 当前插件地址
 _plugin_parm = sys.argv[2]  # 问号以后的内容
 _plugin_dialog = xbmcgui.Dialog()
-print('duola:' + str(_plugin_handle), _plugin_address, _plugin_parm)
+print('duola_debug:[' + str(_plugin_handle)+']'+ _plugin_address+' || '+ _plugin_parm)
 
 # bot config
-Site_url ='https://a1.m1907.cn/api/v/?z=80cbcae8041527fc77204d82c3c725fd&jx='
+Site = 'https://m3u8.xiangkanapi.com'
+Site_api_list = Site + '/provide/vod/?ac=list'
+Site_api_search = Site + '/provide/vod/?wd='
+Site_api_detail = Site + '/provide/vod/?ac=detail&ids='
+
 UA_head = { 
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36',
  }
 
-# 【特定函数】
+# 特定函数
 def check_json(input_str):
     try:
         json.loads(input_str)
         return True
     except:
         return False
+def check_url_mime(url):
+    hz = '.' + url.split('.')[-1]
+    if hz in _plugin_player_mimes:
+        return True
+    else:
+        return False
 
-# 搜索视频
-def Bot_load_search(keyword):
-    so_url = Site_url + keyword + '&s1ig=11400&g='
-    res = requests.get(url=so_url,headers=UA_head)
-    print('duola_debug:'+so_url, 'html_res:'+res.text)
+# 视频查找：返回符合的视频列表
+def Web_load_search(keyword):
+    to_url = Site_api_search + keyword
+    res = requests.get(url=to_url,headers=UA_head)
+    # print('dula_debug:'+to_url, res.text)
     if check_json(res.text):
-        so_json = json.loads(res.text)
-        if so_json['type'] == 'movie' or so_json['type'] == 'tv':
-            if len(so_json['data']) > 0:
-                for vod in so_json['data']:
-                    print(vod['name'], vod['year'])
-                    vod_title = '[COLOR yellow]《' + vod['name'] + '》[/COLOR] '
-                    i = 0
-                    for play in vod['source']['eps']:
-                        i = i + 1
-                        list_item = xbmcgui.ListItem(vod_title + play['name'])
-                        # list_item.setArt({'thumb': '123.JPG'})
-                        # list_item.setInfo('video', {'year': vod['year'], 'title':vod['name'], 'episodeguide': play['name'], 'tracknumber': i})
-                        xbmcplugin.addDirectoryItem(_plugin_handle, play['url'], list_item, False)
-                # xbmcplugin.endOfDirectory(handle=_plugin_handle, succeeded=True, updateListing=False, cacheToDisc=True)
+        res_json = json.loads(res.text)
+        if res_json['code'] == 1:
+            if len(res_json['list']) > 0:
+                for video in res_json['list']:
+                    vod_id = str(video['vod_id'])
+                    vod_name = '[COLOR yellow]' + video['vod_name'] + '[/COLOR] '
+                    vod_remarks = video['vod_remarks']
+                    vod_typename = video['type_name']
+                    # 建立kodi菜单
+                    list_item = xbmcgui.ListItem(vod_name+' ('+ vod_typename+' / '+vod_remarks+')')
+                    # list_item.setArt({'icon': '123.JPG'})
+                    # list_item.setInfo('video', {'year': vod['year'], 'title':vod['name'], 'episodeguide': play['name'], 'tracknumber': i})
+                    xbmcplugin.addDirectoryItem(_plugin_handle, _plugin_address+'?kodi_detail='+vod_id, list_item, True)
+                # 退出kodi菜单布局
+                xbmcplugin.endOfDirectory(handle=_plugin_handle, succeeded=True, updateListing=False, cacheToDisc=True)
             else:
-                print('duola_debug:找不到播放地址')
-                _plugin_dialog.notification(heading=_plugin_name, message='抱歉，找不到播放列表', time=3000)
+                print('duola_debug:找不到资源')
+                _plugin_dialog.notification(heading=_plugin_name, message='抱歉，找不到相关资源', time=3000)
         else:
-            print('duola_debug:找不到内容')
-            _plugin_dialog.notification(heading=_plugin_name, message='抱歉，找不到相关内容', time=3000)
+            print('duola_debug:无法解析json')
+            _plugin_dialog.notification(heading=_plugin_name, message='抱歉，由于无法解析返回的数据，服务暂时不可用，请稍后重试', time=3000)
     else:
         print('duola_debug:目标服务器返回的数据无法解析')
-        _plugin_dialog.notification(heading=_plugin_name, message='抱歉，目标服务器返回的数据无法解析，服务咱不可用', time=3000)
+        _plugin_dialog.notification(heading=_plugin_name, message='抱歉，目标服务器返回的数据无法响应，服务暂不可用', time=3000)
+
+# 视频内容：返回单个视频详情，并交互输出select play list
+def Web_load_detail_one_style1(detail):
+    to_url = Site_api_detail + detail
+    res = requests.get(url=to_url,headers=UA_head)
+    # print('dula_debug:'+to_url, res.text)
+    if check_json(res.text):
+        res_json = json.loads(res.text)
+        if res_json['code'] == 1:
+            if len(res_json['list']) > 0:
+                video = res_json['list'][0] # 仅提取一个
+                v_id = str(video['vod_id'])
+                v_name = '[COLOR yellow]' + video['vod_name'] + '[/COLOR] '
+                v_remarks = video['vod_remarks']
+                v_typename = video['type_name']
+                v_picture = video['vod_pic']
+                # 第1集$https://v.qq.com/x/1.mp4#第2集$https://v.qq.com/x/2.mp4#第3集$https://v.qq.com/x/3.mp4
+                v_list_text = video['vod_play_url'] 
+                v_infos = {}
+                try:
+                    v_infos['title'] = video['vod_name']
+                    v_infos['originaltitle'] = video['vod_name']
+                    v_infos['tag'] = video['vod_remarks']
+                    v_infos['status'] = 'n/a'
+                    v_infos['country'] = video['vod_area']
+                    v_infos['year'] = video['vod_year']
+                    v_infos['director'] = video['vod_director']
+                    v_infos['cast'] = video['vod_actor'].split(',')
+                    v_infos['plot'] = video['vod_content']
+                    v_infos['rating'] = float(video['vod_score'])
+                except IndexError as e:
+                    pass
+                # dialog.select
+                playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+                V_name_list = []
+                V_m3u8_list = []
+                GZ_v = re.compile(r'(.+?)\$(.+?)#')
+                play_list = GZ_v.findall(v_list_text)
+                if len(play_list) > 0:
+                    select_title = v_name + ':请选择播放源开始播放'
+                    i = 0
+                    for play in play_list:
+                        if check_url_mime(play[1]):
+                            V_name_list.append(play[0]) # 播放标签
+                            V_m3u8_list.append(play[1]) # 播放地址
+                            # listitem
+                            list_item = xbmcgui.ListItem(v_name + ':' + play[0], v_typename)
+                            list_item.setArt({'thumb': v_picture, 'poster': v_picture})
+                            list_item.setInfo('video', v_infos)
+                            playlist.add(url=play[1], listitem=list_item, index=i)
+                            i = i +1
+                        else:
+                            pass # 不符合条件的播放地址跳过
+                else:
+                   select_title = '此视频暂时没有播放源'
+                # 生成 select
+                dialog = xbmcgui.Dialog()
+                select_i = dialog.select(select_title, V_name_list)
+                print('duola_debug: select_i '+str(select_i))
+                if select_i >= 0:
+                    # 立即播放单个
+                    # xbmc.Player().play(item=V_m3u8_list[select_i], listitem=list_item)
+                    # 立即播放列表
+                    xbmc.Player().play(item=playlist, listitem=list_item, windowed=False, startpos=select_i)
+                    _plugin_dialog.notification(
+                        heading = _plugin_name, 
+                        message = v_name + ':' + V_name_list[select_i] + ' 即将自动播放，请稍候', 
+                        time=5000, 
+                        sound=False
+                    )
+            else:
+                print('duola_debug:没有数据')
+                _plugin_dialog.notification(heading=_plugin_name, message='抱歉，找不到播放列表', time=3000)
+        else:
+            print('duola_debug:无法解析json')
+            _plugin_dialog.notification(heading=_plugin_name, message='抱歉，由于无法解析返回的数据，服务暂时不可用，请稍后重试', time=3000)
+    else:
+        print('duola_debug:目标服务器返回的数据无法解析')
+        _plugin_dialog.notification(heading=_plugin_name, message='抱歉，目标服务器返回的数据无法响应，服务暂不可用', time=3000)
+
+# 视频内容：返回单个视频详情，并交互输出select play list
+def Web_load_detail_one_style2(detail):
+    to_url = Site_api_detail + detail
+    res = requests.get(url=to_url,headers=UA_head)
+    # print('dula_debug:'+to_url, res.text)
+    if check_json(res.text):
+        res_json = json.loads(res.text)
+        if res_json['code'] == 1:
+            if len(res_json['list']) > 0:
+                video = res_json['list'][0] # 仅提取一个
+                v_id = str(video['vod_id'])
+                v_name = '[COLOR yellow]' + video['vod_name'] + '[/COLOR] '
+                v_remarks = video['vod_remarks']
+                v_typename = video['type_name']
+                v_picture = video['vod_pic']
+                # 第1集$https://v.qq.com/x/1.mp4#第2集$https://v.qq.com/x/2.mp4#第3集$https://v.qq.com/x/3.mp4
+                v_list_text = video['vod_play_url'] 
+                v_infos = {}
+                try:
+                    v_infos['title'] = video['vod_name']
+                    v_infos['originaltitle'] = video['vod_name']
+                    v_infos['tag'] = video['vod_remarks']
+                    v_infos['status'] = 'n/a'
+                    v_infos['country'] = video['vod_area']
+                    v_infos['year'] = video['vod_year']
+                    v_infos['director'] = video['vod_director']
+                    v_infos['cast'] = video['vod_actor'].split(',')
+                    v_infos['plot'] = video['vod_content']
+                    v_infos['rating'] = float(video['vod_score'])
+                except IndexError as e:
+                    pass
+                # A方案：dialog
+                V_name_list = []
+                V_m3u8_list = []
+                GZ_v = re.compile(r'(.+?)\$(.+?)#')
+                play_list = GZ_v.findall(v_list_text)
+                if len(play_list) > 0:
+                    select_title = v_name + ':请选择播放源开始播放'
+                    for play in play_list:
+                        if check_url_mime(play[1]):
+                            V_name_list.append(play[0]) # 播放标签
+                            V_m3u8_list.append(play[1]) # 播放地址
+                        else:
+                            pass # 不符合条件的播放地址跳过
+                else:
+                   select_title = '此视频暂时没有播放源'
+                dialog = xbmcgui.Dialog()
+                select_i = dialog.select(select_title, V_name_list)
+                # 生成 select list
+                print('duola_debug: select_i '+str(select_i))
+                if select_i >= 0:
+                    list_item = xbmcgui.ListItem(v_name, v_typename, V_m3u8_list[select_i], offscreen=False)
+                    list_item.setArt({'thumb': v_picture, 'poster': v_picture})
+                    list_item.setInfo('video', v_infos)
+                    #_plugin_dialog.info(list_item) # 显示视频信息，含播放按钮
+                    xbmc.Player().play(item=V_m3u8_list[select_i], listitem=list_item) # 立即播放
+                    _plugin_dialog.notification(
+                        heading = _plugin_name, 
+                        message = v_name + ':' + V_name_list[select_i] + ' 即将自动播放，请稍候', 
+                        time = 5000, 
+                        sound = False
+                    )
+                # B方案：Directory
+                # list_item = xbmcgui.ListItem(v_name+' (' + v_remarks+' / ' + v_typename + ')')
+                # list_item.setArt({'thumb': v_picture})
+                # list_item.setInfo('video', v_infos)
+                # 建立目录菜单
+                # xbmcplugin.addDirectoryItem(_plugin_handle, _plugin_address+'?kodi_test=123', list_item, True)
+                # 退出目录菜单 布局
+                # xbmcplugin.endOfDirectory(handle=_plugin_handle, succeeded=True, updateListing=False, cacheToDisc=True)
+            else:
+                print('duola_debug:没有数据')
+                _plugin_dialog.notification(heading=_plugin_name, message='抱歉，找不到播放列表', time=3000)
+        else:
+            print('duola_debug:无法解析json')
+            _plugin_dialog.notification(heading=_plugin_name, message='抱歉，由于无法解析返回的数据，服务暂时不可用，请稍后重试', time=3000)
+    else:
+        print('duola_debug:目标服务器返回的数据无法解析')
+        _plugin_dialog.notification(heading=_plugin_name, message='抱歉，目标服务器返回的数据无法响应，服务暂不可用', time=3000)
 
 # 搜索交互
-def bot_start_search():
+def kodi_start_search():
     keyboard = xbmc.Keyboard()
     keyboard.setHeading('请输入关键词')
     keyboard.doModal()
@@ -67,26 +238,35 @@ def bot_start_search():
     if keyboard.isConfirmed():
         keyword = keyboard.getText()
         if len(keyword) < 1:
-            # https://codedocs.xyz/xbmc/xbmc/group__python___dialog.html#gaa2e71498b420cb4f58e82a467f27c659
             msgbox = _plugin_dialog.ok(_plugin_name, '您必须输入关键词才可以搜索相关内容')
     else:
         keyword = ''
     print('duola_debug:' + keyword)
     if len(keyword) > 0:
-        Bot_load_search(keyword)
+        Web_load_search(keyword)
 
-# 当前选择> 首页
+# 当前路由> 访问首页
 if _plugin_parm == '':
     # https://codedocs.xyz/xbmc/xbmc/group__python___dialog.html#ga27157f98dddb21b44cc6456137977aa2
     #_plugin_dialog.notification(_plugin_name,'欢迎使用'+_plugin_name, xbmcgui.NOTIFICATION_INFO, 3000, False)
-    # listitem=xbmcgui.ListItem('[COLOR yellow]哆啦搜索[/COLOR]')
-    # xbmcplugin.addDirectoryItem(_plugin_handle, _plugin_address+'?kodi_search=yes', listitem, True)
-    bot_start_search()
+    listitem=xbmcgui.ListItem('[COLOR yellow]哆啦搜索[/COLOR]')
+    xbmcplugin.addDirectoryItem(_plugin_handle, _plugin_address+'?kodi_search=yes', listitem, True)
+    listitem=xbmcgui.ListItem('使用帮助')
+    xbmcplugin.addDirectoryItem(_plugin_handle, _plugin_address+'?kodi_help', listitem, True)
+    # 退出kodi菜单布局（如果没有及时退出布局会被Kodi进行布局，而不响应文件夹点击）
+    xbmcplugin.endOfDirectory(handle=_plugin_handle, succeeded=True, updateListing=False, cacheToDisc=True)
+    # kodi_start_search()
 
-# 当前选择> 搜索
+# 当前路由> 访问搜索
 if '?kodi_search=yes' in _plugin_parm:
-    print('duola_debug:search>' + _plugin_parm)
-    bot_start_search()
+    print('duola_debug:' + _plugin_parm)
+    kodi_start_search()
 
-# 退出Kodi菜单布局
-xbmcplugin.endOfDirectory(_plugin_handle)
+# 当前路由> 访问视频详情
+if '?kodi_detail=' in _plugin_parm:
+    detail = _plugin_parm.split("kodi_detail=")[1]
+    if detail != "":
+        this_list = Web_load_detail_one_style1(detail)
+    else:
+        print('maliao_debug:传入的kodi_detail地址为空')
+        _plugin_dialog.notification(heading=_plugin_name, message='此视频信息无效，无法访问此视频', time=3000)
--- plugin.video.hanju/main.py
+++ plugin.video.hanju/main.py
@@ -1,24 +1,24 @@
 # -*- coding:utf-8 -*-
-import re
+import sys, re
 import requests
-from collections import OrderedDict
-import xbmcplugin, xbmcgui
 import urllib.parse
+from collections import OrderedDict
+import xbmcplugin, xbmcgui, xbmc
 # https://kodi.wiki/view/Add-on_development
 
 # plugin config
-_handle=int(sys.argv[1])  # 当前句柄
-_plugin_address =sys.argv[0]  # 当前插件地址
-_wh_url =sys.argv[2]  # 问号以后的内容
-print('爬虫调试_cj:' + str(_handle))
-print('爬虫调试_cj_address:' + _plugin_address)
-print('爬虫调试_cj_wh_url:' + _wh_url)
+Plugin_name = '韩剧'
+Plugin_handle = int(sys.argv[1])  # 当前句柄
+Plugin_URL = sys.argv[0]  # 当前插件地址
+Plugin_URL_parm = sys.argv[2]  # 问号以后的内容
+Plugin_dialog = xbmcgui.Dialog()
+print('maliao_debug:' + str(Plugin_handle), Plugin_URL, Plugin_URL_parm)
 
 # api config
-_site ='http://www.hanjutt8.cn'  # 接口域名
-_site_18 = False  # 不良内容拦截开关
-_close_keyword = '伦理片,情色片'  # 不良内容关键词黑名单
-_encoding = 'utf-8'
+Bot_site ='http://www.hanjutt8.cn'  # 接口域名
+Bot_site_18 = False  # 不良内容拦截开关
+Bot_site_blackwords = '伦理片,情色片'  # 不良内容关键词黑名单
+Bot_site_encoding = 'utf-8'
 UA_head = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36',
     "Connection": "close"
@@ -27,15 +27,16 @@ UA_head = {
 # 【生成频道列表】
 def site_make_menu():
     listitem = xbmcgui.ListItem("电影")
-    xbmcplugin.addDirectoryItem(_handle, _plugin_address + '?kodi_type=/vodtype/1.html', listitem, True)
+    xbmcplugin.addDirectoryItem(Plugin_handle, Plugin_URL + '?kodi_type=/vodtype/1.html', listitem, True)
     listitem = xbmcgui.ListItem("电视")
-    xbmcplugin.addDirectoryItem(_handle, _plugin_address + '?kodi_type=/vodtype/15.html', listitem, True)
+    xbmcplugin.addDirectoryItem(Plugin_handle, Plugin_URL + '?kodi_type=/vodtype/15.html', listitem, True)
+    xbmcplugin.endOfDirectory(Plugin_handle) # kodi 文件目录布局结束，必须结束，否则程序不会停止布局
 
 # 【获取频道列表】
 def site_load_menu():
-    url = _site + '/'
+    url = Bot_site + '/'
     res = requests.get(url, headers=UA_head)
-    res.encoding = _encoding
+    res.encoding = Bot_site_encoding
     code = res.text
     re_z = re.compile(r'<li><a.href=./hanju/(.*?).>(.*?)</a>')  # 正则
     types = re_z.findall(code)
@@ -45,24 +46,26 @@ def site_load_menu():
             type_url = '?kodi_type=' + type[0]
             type_name = type[1]
             # KODI代码嵌入开始
-            if _site_18 == True:
-                if type_name in _close_keyword:  # 屏蔽少儿不宜频道
+            if Bot_site_18 == True:
+                if type_name in Bot_site_blackwords:  # 屏蔽少儿不宜频道
                     continue
                 else:
                     listitem = xbmcgui.ListItem(type_name)
-                    xbmcplugin.addDirectoryItem(_handle, _plugin_address + type_url, listitem, True)
+                    xbmcplugin.addDirectoryItem(Plugin_handle, Plugin_URL + type_url, listitem, True)
             else:
                 listitem = xbmcgui.ListItem(type_name)
-                xbmcplugin.addDirectoryItem(_handle, _plugin_address + type_url, listitem, True)
+                xbmcplugin.addDirectoryItem(Plugin_handle, Plugin_URL + type_url, listitem, True)
+            xbmcplugin.endOfDirectory(Plugin_handle)
             # KODI代码嵌入完毕
     else:
         print('maliao_debug:暂无电影分类列表提供')
+        Plugin_dialog.notification(heading=Plugin_name, message='抱歉，找不到播放列表', time=3000)
 
 # 【获取栏目列表】
 def site_load_type(type_url):
-    url = _site + urllib.parse.unquote(type_url)
+    url = Bot_site + urllib.parse.unquote(type_url)
     res = requests.get(url, headers=UA_head)
-    res.encoding = _encoding
+    res.encoding = Bot_site_encoding
     html_code = res.text
     gz = re.compile(r'stui-vodlist__box">\s*<a.+?href="(.+?)".title="(.+?)">\s*<img.+?src="(.+?)".+?>[\s\S]*?pic-text.text-right">(.+?)</span>')
     videos = gz.findall(html_code)
@@ -73,27 +76,27 @@ def site_load_type(type_url):
             if 'http' in video[2]:
                 v_images = video[2]
             else:
-                v_images = _site + video[2]
+                v_images = Bot_site + video[2]
             v_title = video[1]
             v_note = video[3]
             v_name = v_title + '(' + v_note + ')'
-            #KODI代码嵌入开始 thumbnailImage=v_images用法在19版本改变
             listitem = xbmcgui.ListItem(v_name)
             listitem.setArt({'thumb': v_images}) # 此设置方法适用于kodi 19+
-            xbmcplugin.addDirectoryItem(_handle, _plugin_address + v_url, listitem, True)
-            #KODI代码嵌入完毕
+            xbmcplugin.addDirectoryItem(Plugin_handle, Plugin_URL + v_url, listitem, True)
+        xbmcplugin.endOfDirectory(Plugin_handle)
     else:
         print('maliao_debug:暂时无法获取到本栏目下的电影列表')
+        Plugin_dialog.notification(heading=Plugin_name, message='暂时无法获取到本栏目下的电影列表', time=3000)
 
 # 【获取电影播放列表】
 def site_load_vod_detail(detail_url):
     # python2中遍历字典时键值对返回的顺序与存储顺序不同，而python3.6+则更改了字典算法会自动按照存储顺序排序，因此此处定义字典为OrderedDict对象
     play_list = OrderedDict()
-    url = _site + urllib.parse.unquote(detail_url)
+    url = Bot_site + urllib.parse.unquote(detail_url)
     res1 = requests.get(url, headers=UA_head)
-    res1.encoding = _encoding
+    res1.encoding = Bot_site_encoding
     text = res1.text
-    text2 = re.search(r'<ul.class="stui-content__playlist.clearfix".+?</ul>', text)
+    text2 = re.search(r'<ul.class="stui-content__playlist.clearfix".+?</ul>', text) # 缩小搜索范围
     play_text = text2.group()
     gz1 = re.compile(r'<li.><a.href="(.+?)">(.+?)</a>\s*</li>')
     p_lists = gz1.findall(play_text)
@@ -106,14 +109,15 @@ def site_load_vod_detail(detail_url):
             play_list[v_play_title] = v_play_url
     else:
         print('maliao_debug:本视频暂无播放地址')
+        Plugin_dialog.notification(heading=Plugin_name, message='本视频暂无播放地址', time=3000)
         play_list.clear()
     return play_list
 
 # 【获取电影播放绝对地址】
 def site_load_vod_play(play_url):
-    url = _site + urllib.parse.unquote(play_url)
+    url = Bot_site + urllib.parse.unquote(play_url)
     res = requests.get(url, headers=UA_head)
-    res.encoding = _encoding
+    res.encoding = Bot_site_encoding
     code = res.text
     GZ_videos = re.compile(r'vod_name.?=.?\'(.+?)\'.+vod_part.?=.?\'(.+?)\'.+"link_pre":.+?,"url":"(.+)","url_next"')
     bofang = GZ_videos.findall(code)
@@ -124,16 +128,16 @@ def site_load_vod_play(play_url):
             play_m3u8 = b[2].replace('\/', '/') # \/ 替换为 /
             # KODI代码嵌入开始
             listitem = xbmcgui.ListItem(play_names)
-            xbmcplugin.addDirectoryItem(_handle, play_m3u8, listitem, False)
-            # KODI代码嵌入完毕
+            xbmcplugin.addDirectoryItem(Plugin_handle, play_m3u8, listitem, False)
+        xbmcplugin.endOfDirectory(Plugin_handle) # kodi菜单目录结束
     else:
         print('maliao_debug:找不到播放地址')
+        Plugin_dialog.notification(heading=Plugin_name, message='抱歉，暂时无法获取播放地址', time=3000)
 
 # 搜索视频信息
 def site_load_search(keyword):
-    print('maliao_debug: 搜索关键词 ' + keyword)
-    so_url = _site + '/vodsearch/-------------.html?wd=' + keyword + '&submit='
-    zy = requests.post(url=so_url,headers=UA_head)
+    so_url = Bot_site + '/vodsearch/-------------.html?wd=' + keyword + '&submit='
+    zy = requests.get(url=so_url,headers=UA_head) # get or post
     code = zy.text
     gz = re.compile(r'<li.+?class="thumb">.+?href="(.+?)".title="(.+?)".data-original="(.+?)".+?</li>')
     sos = gz.findall(code)
@@ -145,72 +149,78 @@ def site_load_search(keyword):
             if 'http' in so[2]:
                 v_images = so[2]
             else:
-                v_images = _site + so[2]
+                v_images = Bot_site + so[2]
             # 屏蔽少儿不宜内容
-            if _site_18 == True:
-                if v_title in _close_keyword:
+            if Bot_site_18 == True:
+                if v_title in Bot_site_blackwords:
                     continue
                 else:
                     listitem = xbmcgui.ListItem(v_title)
                     listitem.setArt({'thumb': v_images}) # 此设置方法适用于kodi 19+
-                    xbmcplugin.addDirectoryItem(_handle, _plugin_address + v_url, listitem, True)
+                    xbmcplugin.addDirectoryItem(Plugin_handle, Plugin_URL + v_url, listitem, True)
             else:
                 listitem = xbmcgui.ListItem(v_title)
                 listitem.setArt({'thumb': v_images}) # 此设置方法适用于kodi 19+
-                xbmcplugin.addDirectoryItem(_handle, _plugin_address + v_url, listitem, True)
+                xbmcplugin.addDirectoryItem(Plugin_handle, Plugin_URL + v_url, listitem, True)
+        xbmcplugin.endOfDirectory(Plugin_handle) # kodi 文件夹结束
     else:
         print('maliao_debug:搜索不到内容')
+        Plugin_dialog.notification(heading=Plugin_name, message='抱歉，暂时无法找到此内容', time=3000)
 
 # 当前选择>首页，为用户【建立主菜单】
-if _wh_url == '':
+if Plugin_URL_parm == '':
     # 先做一个搜索按钮
     listitem=xbmcgui.ListItem('[COLOR yellow]搜索[/COLOR]')
-    xbmcplugin.addDirectoryItem(_handle, _plugin_address+'?kodi_search=yes', listitem, True)
+    xbmcplugin.addDirectoryItem(Plugin_handle, Plugin_URL+'?kodi_search=yes', listitem, True)
+    xbmcplugin.endOfDirectory(Plugin_handle) # kodi 文件目录布局结束，必须结束，否则程序不会停止布局
     # 载入分类列表，并生成栏目菜单
     site_make_menu()
 
 # 当前选择>搜索按钮位置
-if '?kodi_search=yes' in _wh_url:
-    print('maliao_debug:kodi_search>' + _wh_url)
+if '?kodi_search=yes' in Plugin_URL_parm:
     keyboard = xbmc.Keyboard('', '请输入影片关键词')
     xbmc.sleep(1500)
     keyboard.doModal()
     if (keyboard.isConfirmed()):
         keyword = keyboard.getText()
-        print('maliao_debug:' + keyword)
-        # url = p_url + urllib.quote_plus(keyword.decode('utf-8').encode('gb2312'))
-        site_load_search(keyword)
-
+        if len(keyword) < 1:
+            msgbox = Plugin_dialog.ok(Plugin_name, '您必须输入关键词才可以搜索相关内容')
+        else:
+            site_load_search(keyword)
+        
 # 当前选择>栏目列表位置
-if '?kodi_type=' in _wh_url:
+if '?kodi_type=' in Plugin_URL_parm:
     # ?kodi_type=/type_1.html，由上层构造的kodi url地址，_通过wh_url来辨认
-    print('maliao_debug:kodi_type>' + _wh_url)
-    type_url = _wh_url.split("kodi_type=")[1]
+    type_url = Plugin_URL_parm.split("kodi_type=")[1]
     if type_url !=  "":
         site_load_type(type_url)
     else:
         print('maliao_debug:无法访问栏目')
+        Plugin_dialog.notification(heading=Plugin_name, message='此栏目无效，无法访问此栏目', time=3000)
 
 # 当前选择>为视频详情
-if '?kodi_detail=' in _wh_url:
-    print('maliao_debug:kodi_detail>' + _wh_url)
-    detail_url = _wh_url.split("kodi_detail=")[1]
+if '?kodi_detail=' in Plugin_URL_parm:
+    detail_url = Plugin_URL_parm.split("kodi_detail=")[1]
     if detail_url != "":
         this_list = site_load_vod_detail(detail_url)
+        # print('maliao_debug:',this_list)
         # 生成播放地址列表
         for t_play, t_url in this_list.items():
             listitem = xbmcgui.ListItem(t_play)
-            xbmcplugin.addDirectoryItem(_handle, _plugin_address + t_url, listitem, True)
+            xbmcplugin.addDirectoryItem(Plugin_handle, Plugin_URL + t_url, listitem, True)
+        xbmcplugin.endOfDirectory(Plugin_handle) # kodi 文件目录布局结束，必须结束，否则程序不会停止布局
     else:
         print('maliao_debug:无法访问视频信息')
+        Plugin_dialog.notification(heading=Plugin_name, message='此视频无效，无法访问此视频', time=3000)
 
 # 当前选择>视频播放地址
-if '?kodi_play=' in _wh_url:
-    play_url = _wh_url.split("kodi_play=")[1]
+if '?kodi_play=' in Plugin_URL_parm:
+    play_url = Plugin_URL_parm.split("kodi_play=")[1]
     if play_url != "":
         site_load_vod_play(play_url)
     else:
-        print('maliao_debug:无法获取播放地址')
+        print('maliao_debug:无法获取播放内容')
+        Plugin_dialog.notification(heading=Plugin_name, message='抱歉，暂时无法获取播放内容', time=3000)
 
 # 目录构建完了，退出菜单布局
-xbmcplugin.endOfDirectory(_handle)
+# xbmcplugin.endOfDirectory(Plugin_handle)
